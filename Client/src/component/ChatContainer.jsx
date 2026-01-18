a33c0bfd618edd4d30f40257fbcb11003077dc37
suman basak <gorangobasakpkr@gmail.com>
2026-01-18 21:55:16 +0530

    Making the frontend part for the chat app

---
+++ Client/src/component/ChatContainer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ChatContainer = () => {
+  return (
+    <div>
+        <h1>
+            Chatcointer
+        </h1>
+    </div>
+  )
+}
+
+export default ChatContainer
---
+++ Client/src/pages/HomePage.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import Sidebar from '../component/Sidebar';
+import ChatContainer from '../component/ChatContainer';
+import RightSidebar from '../component/RightSidebar';
+
+const HomePage = () => {
+  const [selecteUser, setSelectedUser] = useState(false);
+
+  return (
+    <div className="border w-full h-screen sm:px-[15%] sm:py-[5%] bg-cover bg-center" style={{ backgroundImage: "url('/path-to-your-image.jpg')" }}>
+      <div
+        className={`backdrop-blur-xl border-2 border-gray-600 rounded-2xl overflow-hidden h-full grid relative ${
+          selecteUser
+            ? 'grid-cols-1 md:grid-cols-[1fr_1.5fr_1fr] xl:grid-cols-[1fr_2fr_1fr]'
+            : 'grid-cols-1 md:grid-cols-2'
+        }`}
+      >
+        <Sidebar selecteUser={selecteUser} setSelectedUser={setSelectedUser} />
+        <ChatContainer selecteUser={selecteUser} setSelectedUser={setSelectedUser}/>
+        <RightSidebar selecteUser={selecteUser} setSelectedUser={setSelectedUser}/>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
