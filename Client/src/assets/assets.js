12cded660c3b2f0e7c3fd426e61e3af8c457f644
suman basak <gorangobasakpkr@gmail.com>
2026-01-20 05:01:29 +0530

    Add the rightside bar, Loginpage, and started the profilepage

--- Client/src/assets/assets.js
+++ Client/src/assets/assets.js
@@ -2,7 +2,7 @@ import avatar_icon from './avatar_icon.png'
 import gallery_icon from './gallery_icon.svg'
 import help_icon from './help_icon.png'
 import logo_icon from './logo_icon.svg'
-import logo_big from './logo_big.svg'
+import logo_big from './image.png'
 import logo from './logo.png'
 import profile_richard from './profile_richard.png'
 import profile_alison from './profile_alison.png'
@@ -48,35 +48,35 @@ export const userDummyData = [
         "email": "test1@greatstack.dev",
         "fullName": "Alison Martin",
         "profilePic": profile_alison,
-        "bio": "Hi Everyone, I am Using QuickChat",
+        "bio": "Hi Everyone, I am Using ChatX",
     },
     {
         "_id": "680f50e4f10f3cd28382ecf9",
         "email": "test2@greatstack.dev",
         "fullName": "Martin Johnson",
         "profilePic": profile_martin,
-        "bio": "Hi Everyone, I am Using QuickChat",
+        "bio": "Hi Everyone, I am Using ChatX",
     },
     {
         "_id": "680f510af10f3cd28382ed01",
         "email": "test3@greatstack.dev",
         "fullName": "Enrique Martinez",
         "profilePic": profile_enrique,
-        "bio": "Hi Everyone, I am Using QuickChat",
+        "bio": "Hi Everyone, I am Using ChatX",
     },
     {
         "_id": "680f5137f10f3cd28382ed10",
         "email": "test4@greatstack.dev",
         "fullName": "Marco Jones",
         "profilePic": profile_marco,
-        "bio": "Hi Everyone, I am Using QuickChat",
+        "bio": "Hi Everyone, I am Using ChatX",
     },
     {
         "_id": "680f516cf10f3cd28382ed11",
         "email": "test5@greatstack.dev",
         "fullName": "Richard Smith",
         "profilePic": profile_richard,
-        "bio": "Hi Everyone, I am Using QuickChat",
+        "bio": "Hi Everyone, I am Using ChatX",
     }
 ]
 
