50e176ab250cdd9878da6f3e1727e91777c7cf80
suman basak <gorangobasakpkr@gmail.com>
2026-01-25 00:35:20 +0530

    Completed the backend

--- server/controllers/messageController.js
+++ server/controllers/messageController.js
@@ -1,9 +1,11 @@
-
-
 // Get all users except the logged in user
-
+import { jsx } from "react/jsx-runtime";
 import Message from "../models/message.js";
 import User from "../models/User.js";
+import cloudary from "../lib/Clodinary.js";
+import { io, userSocketMap } from "../server.js";
+import messageRouter from "../routes/messageRoute.js";
+
 
 export const getUserForSideBar = async (req, res)=>{
     try {
@@ -41,11 +43,81 @@ export const getUserForSideBar = async (req, res)=>{
 export const getMessage = async (req, res)=>{
     try {
         const {id: selectedUserId} = req.params
+        const myId = req.user._id;
+
+        const messages = await Message.find({
+            $or: [
+                {senderId: myId, receiverId: selectedUserId},
+                {senderId: selectedUserId, receiverId: myId},
+            ]
+        }) 
+        await Message.updateMany({senderId: selectedUserId, receiverId: myId}, 
+            {seen: true}
+        )
+
+        res.json({
+            success: true,
+            messages
+        })
+
     } catch (error) {
             console.log(error.messages);
         res.json({
             success: false,
             message: error.message
         })
+    }
+}
+
+
+//Api to mark message as seen using message id
+export const markMessageAsSeen = async (req, res)=>{
+    try {
+        const {id} = req.params;
+        await Message.findByIdAndUpdate(id, {seen: true})
+        res.json({success: true});
+
+        
+    } catch (error) {
+        res.json({
+            success: false,
+            message: error.message
+        })        
+    }
+}
+
+
+//send message to selected user
+
+export const sendMessage = async(req, res) => {
+    try {
+        const {text, image} = req.body;
+        const receiverId = req.params;
+        const senderId = req.user._id;
+
+        let imageUrl;
+        if(image){
+            const uploadResponse = await cloudary.uploader.upload(image);
+            imageUrl = uploadResponse.secure_url;
+        } 
+
+        const newMessage = await Message.create({
+            senderId,
+            receiverId,
+            text,
+            image: imageUrl
+        })
+        
+
+        //emmit the new message to the recevier
+        const receiverScoketId = userSocketMap[receiverId];
+        if(receiverScoketId){
+            io.to(receiverScoketId).emit("newMessage", newMessage)
+        }
+
+        res.json({success:true, newMessage});
+    } catch (error) {
+        console.log(error);
+        res.error.message
     }
 }
--- server/server.js
+++ server/server.js
@@ -5,9 +5,38 @@ import http from "http"
 import { env } from "process"
 import { connectDB } from "./lib/db.js"
 import userRouter from "./routes/userRoutes.js"
+import messageRouter from "./routes/messageRoute.js"
+import { Server } from "socket.io"
+
 //Create The EXpress Server
 const app = express();
 const server = http.createServer(app);
+
+//Initallizer socket.io server
+export const io = new Server(server, {
+    cors: {origin: "*"}
+})
+
+//Store online user
+export const userSocketMap = {}; // {userID: socketId}
+
+
+//soiket.io connecttion handdler
+io.on("connection", (socket)=>{
+    const userId = socket.handshake.query.userId;
+    console.log("User Connected" , userId);
+
+    if(userId) userSocketMap[userId] = socket.id;
+
+    // emmit
+    io.emit("getOnlineUsers", Object.keys(userSocketMap));
+
+    socket.on("disconnect" , ()=>{
+        console.log("User Dissconnected", userId);
+        delete userSocketMap[userId];
+        io.emit("getOnlineUsers", Object.keys(userSocketMap))
+    })
+})
 
 //Create the Middle Ware
 app.use(express.json({limit: "4mb"}));
@@ -17,6 +46,7 @@ app.use(cors());
 //route setup
 app.use("/api/status", (req, res)=> res.send("Server is Live"));
 app.use("/api/auth", userRouter);
+app.use("/api/messages", messageRouter);
 
 //Mongobd 
 await connectDB();
